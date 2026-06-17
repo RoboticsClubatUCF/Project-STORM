002f394855df542713c3ee1845c33ded147b9ed7
devsamuelv <realalloutmusic@gmail.com>
2026-06-17 17:06:09 +0000

    Script was outdated

--- devscripts/build.sh
+++ devscripts/build.sh
@@ -1,3 +1,3 @@
 # !/bin/bash
 
-colcon build --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_CXX_STANDARD=23
+colcon build --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_CXX_STANDARD=20
